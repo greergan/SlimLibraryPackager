427f3f1292c6830295e50c05f33f9821765cab4e
Jeff Greer <greergan@gmail.com>
2026-04-29 21:04:32 -0700

    generate_main_cpp only works for SlimCommon

--- cmake/SlimSourceFunctions.cmake
+++ cmake/SlimSourceFunctions.cmake
@@ -1,38 +1,43 @@
 function(generate_main_cpp)
-    if(NOT SLIM_USE_LOCAL_SOURCE)
-        message(STATUS "generate_main_cpp: skipped (SLIM_USE_LOCAL_SOURCE is OFF)")
-        return()
-    endif()
- 
     get_primary_module(_primary)
     if(NOT _primary)
         message(FATAL_ERROR "generate_main_cpp: no primary module defined")
     endif()
- 
+
+    if(NOT "${_primary}" STREQUAL "SlimCommon")
+        message(STATUS "generate_main_cpp: skipped (primary module is '${_primary}', not 'SlimCommon')")
+        return()
+    endif()
+
+    if(NOT SLIM_USE_LOCAL_SOURCE)
+        message(STATUS "generate_main_cpp: skipped (SLIM_USE_LOCAL_SOURCE is OFF)")
+        return()
+    endif()
+
     meta_get(MODULE "${_primary}" src_dir _src_dir)
- 
+
     set(_includes "")
     foreach(_name IN LISTS MODULE_NAMES)
         meta_get(MODULE "${_name}" primary _is_primary)
         if(_is_primary)
             continue()
         endif()
- 
+
         meta_get(MODULE "${_name}" header_file_out _hdr)
         if(NOT _hdr)
             message(WARNING "generate_main_cpp: module '${_name}' has no header_file_out, skipping")
             continue()
         endif()
- 
+
         string(REGEX REPLACE "^include/" "" _hdr_rel "${_hdr}")
         list(APPEND _includes "#include <${_hdr_rel}>")
     endforeach()
- 
+
     if(NOT _includes)
         message(WARNING "generate_main_cpp: no sub-module headers found, aborting")
         return()
     endif()
- 
+
     list(JOIN _includes "\n" _includes_block)
     set(_out_src "${_src_dir}/src/main.cpp")
     file(WRITE "${_out_src}" "${_includes_block}\n")
